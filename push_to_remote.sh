0b2b461626a8b5c9fd20e96e14185acd20f6594a
Alina Potapova <potapovaalina935@gmail.com>
2022-07-06 10:37:20 +0200

    unzip is added

--- push_to_remote.sh
+++ push_to_remote.sh
@@ -5,4 +5,4 @@
 rm t-shirt-sizes/src/back/node_modules
 # zip project folder with all files inside (-r is recursive)
 zip build t-shirt-sizes/* -r 
-scp build.zip root@159.69.216.58:~   
+scp build.zip t-shirt-sizes/unzip.sh root@159.69.216.58:~   
